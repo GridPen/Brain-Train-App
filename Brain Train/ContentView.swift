c1fdb197a8fedf2a661ea7a399d483fed6a5d777
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-02-16 00:51:08 +0600

    Stats

--- Brain Train/ContentView.swift
+++ Brain Train/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rock = 0
+    @State private var paper = 0
+    @State private var scissors = 0
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .foregroundColor(.yellow)
+            .background(.red)
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
