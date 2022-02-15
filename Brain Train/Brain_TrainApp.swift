e992d2e9fad406fa6d6968272b9adafa90be4690
Debashish M <10178321+GridPen@users.noreply.github.com>
2022-02-15 23:48:01 +0600

    Initial Commit

---
+++ Brain Train/Brain_TrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brain_TrainApp.swift
+//  Brain Train
+//
+//  Created by Debashish Mondal on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Brain_TrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
